1fa4efc648cc69cc537ce92c95bc705f940f9567
Daniel Errante <danoph@gmail.com>
2018-09-13 09:44:25 -0500

    update starting specs

--- spec/gilded.rose.spec.ts
+++ spec/gilded.rose.spec.ts
@@ -3,36 +3,33 @@ import { Item, GildedRose } from '../src/gilded.rose';
 describe('GildedRose', () => {
   let subject;
 
-  let items;
-
   let dexterityVest;
   let agedBrie;
   let conjuredManaCake;
+  let conjuredLavaCake;
   let sulfuras;
   let backstagePasses;
 
-  beforeEach(() => {
-    dexterityVest = new Item("+5 Dexterity Vest", 10, 20);
-    conjuredManaCake = new Item("Conjured Mana Cake", 3, 6);
-    backstagePasses = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 15);
-    agedBrie = new Item("Aged Brie", 4, 11)
-    sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
-
-    items = [
-      dexterityVest,
-      backstagePasses,
-      sulfuras,
-      agedBrie,
-      conjuredManaCake
-    ]
-
-    subject = new GildedRose(items);
-  })
-
   describe('#updateQuality', () => {
-    it('does something', () => {
-      //console.log('before updating quality', subject.items);
+    beforeEach(() => {
+      dexterityVest = new Item("+5 Dexterity Vest", 10, 20);
+      conjuredManaCake = new Item("Conjured Mana Cake", 3, 6);
+      conjuredLavaCake = new Item("Conjured Lava Cake", 2, 7);
+      backstagePasses = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 15);
+      agedBrie = new Item("Aged Brie", 4, 11);
+      sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
 
+      subject = new GildedRose([
+        dexterityVest,
+        backstagePasses,
+        sulfuras,
+        agedBrie,
+        //conjuredManaCake,
+        //conjuredLavaCake
+      ]);
+    })
+
+    it('updates items correctly', () => {
       subject.updateQuality();
 
       expect(dexterityVest.sellIn).toEqual(9)
@@ -47,11 +44,240 @@ describe('GildedRose', () => {
       expect(agedBrie.sellIn).toEqual(3);
       expect(agedBrie.quality).toEqual(12);
 
-      expect(conjuredManaCake.sellIn).toEqual(2);
-      expect(conjuredManaCake.quality).toEqual(4);
+      //expect(conjuredManaCake.sellIn).toEqual(2);
+      //expect(conjuredManaCake.quality).toEqual(4);
 
-      //console.log('');
-      //console.log('after updating quality', subject.items);
+      //expect(conjuredLavaCake.sellIn).toEqual(1);
+      //expect(conjuredLavaCake.quality).toEqual(5);
     });
+
+    describe('dexterity vest', () => {
+      describe('has sellIn of -1', () => {
+        beforeEach(() => {
+          dexterityVest.sellIn = -1
+        })
+
+        it('degrades quality by 2', () => {
+          subject.updateQuality();
+          expect(dexterityVest.quality).toEqual(18)
+        })
+      })
+    })
+
+    //describe('conjured mana cake', () => {
+      //describe('has quality of 1', () => {
+        //beforeEach(() => {
+          //conjuredManaCake.quality = 1
+        //})
+
+        //it('does not decrease quality below 0', () => {
+          //subject.updateQuality();
+          //expect(conjuredManaCake.quality).toEqual(0)
+        //})
+      //})
+
+      //describe('has sellIn date of -1', () => {
+        //beforeEach(() => {
+          //conjuredManaCake.sellIn = -1
+        //})
+
+        //describe('and quality is 6', () => {
+          //beforeEach(() => {
+            //conjuredManaCake.quality = 6;
+          //})
+
+          //it('quality should be 2', () => {
+            //subject.updateQuality();
+
+            //expect(conjuredManaCake.sellIn).toEqual(-2)
+            //expect(conjuredManaCake.quality).toEqual(2)
+          //})
+        //})
+
+        //describe('and quality is 0', () => {
+          //beforeEach(() => {
+            //conjuredManaCake.quality = 0;
+          //})
+
+          //it('does not decrease quality below 0', () => {
+            //subject.updateQuality();
+
+            //expect(conjuredManaCake.sellIn).toEqual(-2)
+            //expect(conjuredManaCake.quality).toEqual(0)
+          //})
+        //})
+      //})
+
+      ////describe('has sellIn date of 0', () => {
+        ////beforeEach(() => {
+          ////conjuredManaCake.sellIn = 0
+        ////})
+
+        ////describe('and quality is 6', () => {
+          ////beforeEach(() => {
+            ////conjuredManaCake.quality = 6;
+          ////})
+
+          ////it('does not decrease quality below 0', () => {
+            ////subject.updateQuality();
+
+            ////expect(conjuredManaCake.sellIn).toEqual(-1)
+            ////expect(conjuredManaCake.quality).toEqual(0)
+          ////})
+        ////})
+      ////})
+    //})
+
+    //describe('conjured lava cake', () => {
+      //describe('has quality of 1', () => {
+        //beforeEach(() => {
+          //conjuredLavaCake.quality = 1
+        //})
+
+        //it('does not decrease quality below 0', () => {
+          //subject.updateQuality();
+          //expect(conjuredLavaCake.quality).toEqual(0)
+        //})
+      //})
+
+      //describe('has sellIn date of -1', () => {
+        //beforeEach(() => {
+          //conjuredLavaCake.sellIn = -1
+        //})
+
+        //describe('and quality is 6', () => {
+          //beforeEach(() => {
+            //conjuredLavaCake.quality = 6;
+          //})
+
+          //it('quality should be 2', () => {
+            //subject.updateQuality();
+
+            //expect(conjuredLavaCake.sellIn).toEqual(-2)
+            //expect(conjuredLavaCake.quality).toEqual(2)
+          //})
+        //})
+
+        //describe('and quality is 0', () => {
+          //beforeEach(() => {
+            //conjuredLavaCake.quality = 0;
+          //})
+
+          //it('does not decrease quality below 0', () => {
+            //subject.updateQuality();
+
+            //expect(conjuredLavaCake.sellIn).toEqual(-2)
+            //expect(conjuredLavaCake.quality).toEqual(0)
+          //})
+        //})
+      //})
+
+      ////describe('has sellIn date of 0', () => {
+        ////beforeEach(() => {
+          ////conjuredLavaCake.sellIn = 0
+        ////})
+
+        ////describe('and quality is 6', () => {
+          ////beforeEach(() => {
+            ////conjuredLavaCake.quality = 6;
+          ////})
+
+          ////it('does not decrease quality below 0', () => {
+            ////subject.updateQuality();
+
+            ////expect(conjuredLavaCake.sellIn).toEqual(-1)
+            ////expect(conjuredLavaCake.quality).toEqual(0)
+          ////})
+        ////})
+      ////})
+    //})
+
+    describe('aged brie', () => {
+      describe('quality is 45', () => {
+        beforeEach(() => {
+          agedBrie.quality = 45
+        })
+
+        describe('sellIn date is 4', () => {
+          beforeEach(() => {
+            agedBrie.sellIn = 4;
+          })
+
+          it('increases quality to 46', () => {
+            subject.updateQuality();
+
+            expect(agedBrie.sellIn).toEqual(3)
+            expect(agedBrie.quality).toEqual(46)
+          })
+        })
+
+        describe('sellIn date is -1', () => {
+          beforeEach(() => {
+            agedBrie.sellIn = -1;
+          })
+
+          it('increases quality to 47', () => {
+            subject.updateQuality();
+
+            expect(agedBrie.sellIn).toEqual(-2)
+            expect(agedBrie.quality).toEqual(47)
+          })
+        })
+      })
+
+      describe('has quality of 50', () => {
+        beforeEach(() => {
+          agedBrie.quality = 50
+        })
+
+        it('does not decrease quality below 0', () => {
+          subject.updateQuality();
+
+          expect(agedBrie.sellIn).toEqual(3)
+          expect(agedBrie.quality).toEqual(50)
+        })
+      })
+    })
+
+    describe('backstage passes', () => {
+      describe('has sellIn of 10', () => {
+        beforeEach(() => {
+          backstagePasses.sellIn = 10
+        })
+
+        it('increases quality by 2', () => {
+          subject.updateQuality();
+
+          expect(backstagePasses.sellIn).toEqual(9)
+          expect(backstagePasses.quality).toEqual(17)
+        })
+      })
+
+      describe('has sellIn of 20', () => {
+        beforeEach(() => {
+          backstagePasses.sellIn = 20
+        })
+
+        it('increases quality by 1', () => {
+          subject.updateQuality();
+
+          expect(backstagePasses.sellIn).toEqual(19)
+          expect(backstagePasses.quality).toEqual(16)
+        })
+      })
+
+      describe('has sellIn of -1', () => {
+        beforeEach(() => {
+          backstagePasses.sellIn = -1
+        })
+
+        it('sets quality to 0', () => {
+          subject.updateQuality();
+
+          expect(backstagePasses.sellIn).toEqual(-2)
+          expect(backstagePasses.quality).toEqual(0)
+        })
+      })
+    })
   })
 })
